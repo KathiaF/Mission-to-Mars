a1c137eb3b036e71ef2febf577187ced1979b919
KathF <kathia.floresrd@uanl.edu.com>
2022-03-14 01:26:15 -0600

    module 10 challenge

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_urls(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,29 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemisphere_urls(browser):
+    hemisphere_image_urls = []
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(4):
+        hemisphere = {}
+        browser.find_by_css('a.product-item img')[i].click()
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemisphere['img_url'] = sample_elem['href']
+        hemisphere['title'] = browser.find_by_css("h2.title").text
+        hemisphere_image_urls.append(hemisphere)
+        
+        browser.back()
+    
+    return hemisphere_image_urls
+
 
 if __name__ == "__main__":
 
